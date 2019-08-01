d30a65dd86a17b1c73442021f9cd47b4c0319e41
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-08-01 13:34:15 +0900

    Updates

---
+++ precache-manifest.94fbc1b26c35401a7a6e95dbef6179e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f8c839ebeb1f9af0fd16",
+    "url": "/js/chunk-vendors.5c5bff03.js"
+  },
+  {
+    "revision": "4a64b48a90e24b74a724",
+    "url": "/js/app.8d8198a8.js"
+  },
+  {
+    "revision": "643473c1552a0a9c2b89",
+    "url": "/js/about.4aafa82e.js"
+  },
+  {
+    "revision": "51d090e7cc37d694a624420b56c584b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "f8c839ebeb1f9af0fd16",
+    "url": "/css/chunk-vendors.70cb4902.css"
+  }
+];
