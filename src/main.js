c5d6fa6e06596fd97441bb2a9997f9ec0fc7ffc8
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-08-04 11:13:46 +0900

    github pages で動作するように変更

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from './router';
 import store from './store';
 import './registerServiceWorker';
 import vuetify from './plugins/vuetify';
-import * as firebase from 'firebase';
 
 Vue.config.productionTip = false;
 
@@ -15,34 +14,11 @@ new Vue({
   render: h => h(App),
 }).$mount('#app');
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyDlAWtdswE7S_jUzNrnna3h7V5ariXi_e8',
-  authDomain: 'task-cabinet.firebaseapp.com',
-  databaseURL: 'https://task-cabinet.firebaseio.com',
-  projectId: 'task-cabinet',
-  storageBucket: '',
-  messagingSenderId: '1088200976068',
-  appId: '1:1088200976068:web:92569e254386d831',
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-const messaging = firebase.messaging();
-
-messaging.usePublicVapidKey(
-  'BKWGEMGWg78ApjGMWvx8YY0IwTBgJhwnFZDbHfWliWOn7RRVSCVh40SjNnYS18ManOz7uc3nfLTHvjslBvigiM0'
-);
-
-// 通知の受信許可
-Notification.requestPermission()
-  .then(() => {
+// Request permission to receive notifications
+Notification.requestPermission().then((permission) => {
+  if (permission === 'granted') {
     console.log('Notification permission granted.');
-
-    // トークン取得
-    messaging.getToken().then(token => {
-      console.log(token);
-    });
-  })
-  .catch(err => {
-    console.log('Unable to get permission to notify.', err);
-  });
+  } else {
+    console.log('Unable to get permission to notify.');
+  }
+});
--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,8 +1,31 @@
 /* eslint-disable no-console */
 
 import { register } from 'register-service-worker';
+import firebase from "firebase";
 
 if (process.env.NODE_ENV === 'production') {
+  // Add Firebase to your JavaScript project
+  // https://firebase.google.com/docs/web/setup
+  const firebaseConfig = {
+    apiKey: 'AIzaSyDlAWtdswE7S_jUzNrnna3h7V5ariXi_e8',
+    authDomain: 'task-cabinet.firebaseapp.com',
+    databaseURL: 'https://task-cabinet.firebaseio.com',
+    projectId: 'task-cabinet',
+    storageBucket: '',
+    messagingSenderId: '1088200976068',
+    appId: '1:1088200976068:web:92569e254386d831',
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  // Set up a JavaScript Firebase Cloud Messaging client app
+  // https://firebase.google.com/docs/cloud-messaging/js/client
+  const messaging = firebase.messaging();
+
+  messaging.usePublicVapidKey(
+    'BKWGEMGWg78ApjGMWvx8YY0IwTBgJhwnFZDbHfWliWOn7RRVSCVh40SjNnYS18ManOz7uc3nfLTHvjslBvigiM0'
+  );
+
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready() {
       console.log(
@@ -10,8 +33,13 @@ if (process.env.NODE_ENV === 'production') {
           'For more details, visit https://goo.gl/AFskqB'
       );
     },
-    registered() {
+    registered(registration) {
       console.log('Service worker has been registered.');
+
+      messaging.useServiceWorker(registration);
+      messaging.getToken().then(token => {
+        console.log(token);
+      });
     },
     cached() {
       console.log('Content has been cached for offline use.');
